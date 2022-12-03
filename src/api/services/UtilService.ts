46b420b2d2a60a6fdd0ebd221c12f14ecb5124bc
gouravnaik <contact@gouravnaik.dev>
2022-12-04 01:34:39 +0530

    infura for ipfs

--- src/api/services/UtilService.ts
+++ src/api/services/UtilService.ts
@@ -1,6 +1,7 @@
-import {Service} from 'typedi';
 import got from 'got';
-import {env} from '../../env';
+import { Service } from 'typedi';
+
+import { env } from '../../env';
 
 @Service()
 export class UtilService {
@@ -15,8 +16,28 @@ export class UtilService {
         };
         let result;
         try {
-            result = await got.post(`${url}`, options)
-                .json();
+            result = await got.post(`${url}`, options).json();
+        } catch (err) {
+            throw new Error(`IPFS upload error`);
+        }
+        return {
+            success: true,
+            url: `ipfs://${result?.IpfsHash}`,
+        };
+    }
+
+    public async uploadMetaData(body: any): Promise<object> {
+        const url = `https://ipfs.infura.io:5001/api/v0/add?pin=true`;
+        const options = {
+            json: body,
+            headers: {
+                Authorization:
+                    'Basic MklQdGdxRlBLaGFOMDVzVVFDc1dUUzdmRUI4OmM0ZTFlYTgzYjE2YjkwZjE0MDY4MmFlYTYzYjQ2YTkw',
+            },
+        };
+        let result;
+        try {
+            result = await got.post(`${url}`, options).json();
         } catch (err) {
             throw new Error(`IPFS upload error`);
         }
